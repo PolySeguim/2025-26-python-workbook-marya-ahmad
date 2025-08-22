c2b88f326e264b72f93fc5ad67a8034263b4a266
marya-ahmad <mza206884@gmail.com>
2025-08-22 15:19:32 +0000

    Main

---
+++ unit2notes.py
@@ -0,0 +1,25 @@
+#def is the keyword to define a function
+#whatType is the name of the function
+#userInput is the parameter of the function
+def whatType(userInput):
+    #print is a python built in function that prints to the console
+    #type is a python built in function that tells us the data type of the variable or value
+    #userInput is the variable that the user enters
+    #print is a built in function that outputs to the screen
+    print(type(userInput))
+
+# The pound symbol is for one line comments
+#The program ignores all comments
+
+"""
+multiple line comments 
+"""
+
+#Call the function with different user inputs
+whatType(3)
+whatType(3.0)
+whatType(True)
+whatType("marya")
+whatType("p")
+
+
